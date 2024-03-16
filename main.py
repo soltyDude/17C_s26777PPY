ede32ab4a7aca632ed5420d99c78584e096ed750
s26777 <s26777@pjwstk.edu.pl>
2024-03-16 21:00:02 +0300

    EX 4

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+import math
+
 class SquareGenerator:
     def generate_squares(self, start, end):
-        return [x**2 for x in range(start, end+1)]
+        """
+        Generates a list of squares for numbers in the given range [start, end].
+        """
+        return [x**2 for x in range(start, end + 1)]
 
-generator = SquareGenerator()
-start = 1
-end = 10
-squares = generator.generate_squares(start, end)
-print(squares)
+# Create an instance of the SquareGenerator class
+square_gen = SquareGenerator()
+
+# Generate squares from 1 to 10
+squares_list = square_gen.generate_squares(1, 10)
+
+# Calculate the square root of each number in the squares list
+square_roots = [math.sqrt(number) for number in squares_list]
+
+# Print the list of squares
+print("Squares list:", squares_list)
+
+# Print the list of square roots
+print("Square roots list:", square_roots)
