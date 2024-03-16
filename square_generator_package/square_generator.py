6f1d4cacc83a0e449bdf539614a796b08f7fb6ba
s26777 <s26777@pjwstk.edu.pl>
2024-03-16 21:09:26 +0300

    EX 8

--- square_generator_package/square_generator.py
+++ square_generator_package/square_generator.py
@@ -1,8 +1,18 @@
-# File: square_generator.py
-import math
-
 class SquareGenerator:
     def generate_squares(self, start, end):
         if end < start:
             return []
         return [x**2 for x in range(start, end + 1)]
+
+class CubicGenerator(SquareGenerator):
+    def generate_cubes(self, start, end):
+        if end < start:
+            return []
+        return [x**3 for x in range(start, end + 1)]
+
+# Example usage of the CubicGenerator subclass
+cubic_gen = CubicGenerator()
+
+# Generate cubes from 1 to 5
+cubes_list = cubic_gen.generate_cubes(1, 5)
+print(cubes_list)
