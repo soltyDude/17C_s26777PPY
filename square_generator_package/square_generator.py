4d0181f24aab73a7b64929e71969751ed9658408
s26777 <s26777@pjwstk.edu.pl>
2024-03-16 21:15:31 +0300

    EX 9

--- square_generator_package/square_generator.py
+++ square_generator_package/square_generator.py
@@ -5,14 +5,14 @@ class SquareGenerator:
         return [x**2 for x in range(start, end + 1)]
 
 class CubicGenerator(SquareGenerator):
-    def generate_cubes(self, start, end):
+    def generate_squares(self, start, end):
         if end < start:
-            return []
-        return [x**3 for x in range(start, end + 1)]
+            raise ValueError("End of range must be greater than or equal to start.")
+        return [x**2 for x in range(start, end + 1)]
 
 # Example usage of the CubicGenerator subclass
 cubic_gen = CubicGenerator()
 
-# Generate cubes from 1 to 5
-cubes_list = cubic_gen.generate_cubes(1, 5)
-print(cubes_list)
+# Generate squares from 1 to 5
+squares_list = cubic_gen.generate_squares(1, 5)
+print(squares_list)
