400f20af188890413f837a2e51e6c5a1e26da2da
s26777 <s26777@pjwstk.edu.pl>
2024-03-16 20:28:32 +0300

    EX 2

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-squares = [x**2 for x in range(1, 11)]
 
+def generate_squares(start, end):
+
+    return [x ** 2 for x in range(start, end + 1)]
+
+
+start = 1
+end = 10
+squares = generate_squares(start, end)
 print(squares)
