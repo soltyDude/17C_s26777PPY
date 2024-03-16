cb5adf78291b51a18133ab2293eb2941b2680a10
s26777 <s26777@pjwstk.edu.pl>
2024-03-16 20:28:46 +0300

    EX 3

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        return [x**2 for x in range(start, end+1)]
 
-def generate_squares(start, end):
-
-    return [x ** 2 for x in range(start, end + 1)]
-
-
+generator = SquareGenerator()
 start = 1
 end = 10
-squares = generate_squares(start, end)
+squares = generator.generate_squares(start, end)
 print(squares)
